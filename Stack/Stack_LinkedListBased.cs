9551d340d2fb2dd10306398f0eff4395eb8fcc6a
MuhammadFathiAli <muhammad.fathi64@gmail.com>
2023-07-03 04:09:33 +0300

    Stack (LinkedListe Based) Implementation

---
+++ Stack/Stack_LinkedListBased.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testConsole
+{
+    public class Stack_LinkedListBased<T> where T : struct
+    {
+        public LinkedList<T> list { private get; private set; }
+        public Stack_LinkedListBased()
+        {
+            list = new LinkedList<T>(true);
+        }
+        public void Push(T _data)
+        {
+            list.InsertFirst(_data);
+        }
+        public T Pop()
+        {
+            T data = list.head.data;
+            list.DeleteHead();
+            return data;
+        }
+        public T Peek()
+        {
+            return list.head.data;
+        }
+        public bool IsEmpty()
+        {
+            return list.Length <= 0;
+        }
+        public int Size()
+        {
+            return list.Length;
+        }
+        public void Print()
+        {
+            list.PrintList();
+        }
+
+    }
+}
