c7bee804b2de3ca786ab0a1da799b92827956650
MuhammadFathiAli <muhammad.fathi64@gmail.com>
2023-07-03 16:41:57 +0300

    Queue Linked List Based Implementation

---
+++ Queue/Queue_LinkedListImp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testConsole
+{
+    public class Queue_LinkedListBased<T> where T : struct
+    {
+        public LinkedList<T> list { get; private set; }
+        public Queue_LinkedListBased()
+        {
+            list = new LinkedList<T>(true);
+        }
+        public void Enqueue(T _data)
+        {
+            list.InsertLast(_data);
+        }
+        public T Dequeue()
+        {
+            T data = list.head.data;
+            list.DeleteHead();
+            return data;
+        }
+        public T Peek()
+        {
+            return list.head.data;
+        }
+        public bool IsEmpty()
+        {
+            return list.Length <= 0;
+        }
+        public int Size()
+        {
+            return list.Length;
+        }
+        public void Print()
+        {
+            list.PrintList();
+        }
+
+    }
+}
